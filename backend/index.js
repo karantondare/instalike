b66828b8a2d1d0f214850aaa81ce0cb17e897a71
unknown <karanstondare999@gmail.com>
2021-04-07 06:54:58 +0530

    .env

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import morgan from "morgan";
 
 import postRoutes from "./routes/postsRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -11,7 +10,6 @@ const app = express();
 dotenv.config();
 
 app.use(cors());
-app.use(morgan("combined"));
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -20,7 +18,7 @@ app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Hello To Instalike Social");
+  res.send("Welcome To Instalike Social");
 });
 
 const PORT = process.env.PORT || 5000;
